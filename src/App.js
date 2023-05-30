d2662c5842eab715cdae30491766d84b01980f96
Pamela Jane <pamelajane@fancyclam.local>
2023-05-30 11:26:32 -0700

    functioning routes were added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
-import React, { useState } from 'react';
-import SearchBar from './components/SearchBar';
-import RepoList from './components/ReproList';
-import CommitList from './components/CommitList';
-import axios from 'axios';
-
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SearchBar from "./components/SearchBar";
+import RepoList from "./components/ReproList";
+import CommitList from "./components/CommitList";
+import axios from "axios";
 
 const App = () => {
   const [repos, setRepos] = useState([]);
   const [commits, setCommits] = useState([]);
-  const [orgName, setOrgName] = useState('Netflix');
-
+  const [orgName, setOrgName] = useState("Netflix");
 
   const handleSearch = (data, org) => {
     setRepos(data);
     setOrgName(org);
-  }
+  };
 
   const handleRepoClick = async (repoName) => {
     try {
-      const response = await axios.get(`https://api.github.com/repos/${orgName}/${repoName}/commits`);
+      const response = await axios.get(
+        `https://api.github.com/repos/${orgName}/${repoName}/commits`
+      );
       setCommits(response.data);
-      console.log(response.data)
+      console.log(response.data);
     } catch (err) {
       console.error(err);
     }
-  }
-
-  
+  };
 
   return (
-    <div>
+    <Router>
       <SearchBar onSearch={handleSearch} />
-      <RepoList repos={repos} onRepoClick={handleRepoClick} />
-      <CommitList commits={commits} />
-
-    </div>
+      <Routes>
+        <Route
+          path="/repo/:repoName/*"
+          element={<CommitList commits={commits} />}
+        />
+        <Route
+          path="/*"
+          element={ <RepoList repos={repos} onRepoClick={handleRepoClick} />}
+        />
+      </Routes>
+    </Router>
   );
 };
 
--- src/components/ReproList.js
+++ src/components/ReproList.js
@@ -1,18 +1,24 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
-import { List, Divider } from '@mui/material';
-import RepoListItem from './ReproListItem';
-
+import React from "react";
+import { List, Divider } from "@mui/material";
+import RepoListItem from "./ReproListItem";
+import { Link } from "react-router-dom";
 
 const ReproList = ({ repos, onRepoClick }) => {
   return (
     <List>
       {repos
-        .sort((a, b) => b.stargazers_count - a.stargazers_count)  // sort by star count
+        .sort((a, b) => b.stargazers_count - a.stargazers_count) // sort by star count
         .map((repo) => (
           <>
-            <RepoListItem key={repo.id} repo={repo} onRepoClick={onRepoClick} />
-            <Divider />
+            <Link to={`/repo/${repo.name}`} style={{ textDecoration: "none" }}>
+              <RepoListItem
+                key={repo.id}
+                repo={repo}
+                onRepoClick={onRepoClick}
+              />
+              <Divider />
+            </Link>
           </>
         ))}
     </List>
